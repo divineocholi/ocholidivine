f531eb627e36aac2f9cb74f1a8e165cafe8be196
ocholi divine <ocholidivine41@gmail.com>
2025-09-05 04:02:11 +0100

    updated navbar

--- js/navbar.js
+++ js/navbar.js
@@ -6,12 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // Toggle menu when hamburger is clicked
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    hamburger.classList.toggle('toggle'); // <-- add this
   });
 
   // Close menu when any link is clicked
   links.forEach(link => {
     link.addEventListener('click', () => {
       navLinks.classList.remove('active');
+      hamburger.classList.remove('toggle'); // <-- also reset the icon
     });
   });
 });
